90b36e7864046b1c8c46e26aaac6eccc3441a07c
Sara Fojt <01169167@pw.edu.pl>
2023-12-07 02:51:50 +0100

    Add database connection - app is connected to mySQL database - it is NOT displaying data from database

--- api/project/settings.py
+++ api/project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'project',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'FitCraftersDatabase',
+        'USER': 'pc',
+        'PASSWORD': 'pc',
+        'HOST':'20.199.51.93',
+        'PORT':'3306',
     }
 }
 

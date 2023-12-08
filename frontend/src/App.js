ed3e3a6ed3eba2b69c08221f3d1cf0658350ba55
Sara Fojt <01169167@pw.edu.pl>
2023-12-09 00:05:55 +0100

    Add important files, create first component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import Navbar from "./components/Navbar"
 import './App.css';
 
 function App() {
-  const [managerName, setManagerName] = useState("");
-
-  useEffect(() => {
-    fetch('http://127.0.0.1:8000/').then(res => res.json()).then(data => {
-      setManagerName(data.manager_name);
-    });
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>The manager's name is {managerName}.</p> <br/>
-      </header>
-    </div>
-  );
+  return <Navbar />;
 }
 
 export default App;
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,11 @@
+function MenuBar() {
+    return <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+    <div class="container-fluid">
+      <span class="navbar-text">
+        FitCrafters
+      </span>
+    </div>
+  </nav>
+}
+
+export default MenuBar
